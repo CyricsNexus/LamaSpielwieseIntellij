066c7b271800ebc0d02187b259edb19c877b861c
Vincent <vincent.brakensiek24@googlemail.com>
2026-03-17 10:24:41 +0100

    Test,Test,testen

--- src/Lama.java
+++ src/Lama.java
@@ -1,10 +1,31 @@
 public class Lama{
     private String tiername;
-    private String tierlaut = "Möööht";
+    private String tierlaut;
 
-    public Lama(){
-        System.out.println("Lama erstellt");
-        System.out.println(tierlaut);
+    public Lama(String tiername, String tierlaut){
+        this.tiername = tiername;
+        this.tierlaut = tierlaut;
+    }
+    public void test(){
+        System.out.println("Hallo welt");
+    }
+    public void ausgebenmuuhlaut(){
+        System.out.println("Muuuh");
     }
 
+    public String getTierlaut() {
+        return tierlaut;
+    }
+
+    public void setTierlaut(String tierlaut) {
+        this.tierlaut = tierlaut;
+    }
+
+    public String getTiername() {
+        return tiername;
+    }
+
+    public void setTiername(String tiername) {
+        this.tiername = tiername;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main() {
-        Lama lama1 = new Lama();
+        Lama lama1 = new Lama("Olga", "Mähh");
+        System.out.println(lama1.getTierlaut());
+        System.out.println(lama1.getTiername());
+        lama1.ausgebenmuuhlaut();
+        lama1.test();
     }
 
 }
